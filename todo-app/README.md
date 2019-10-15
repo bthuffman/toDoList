faa21253e9ac3fde0adc02bed4de971a98190601
Brandon Huffman <bt_huffman@msn.com>
2019-10-15 14:59:51 -0600

    Minor changes

--- todo-app/README.md
+++ todo-app/README.md
@@ -1,3 +1,5 @@
+https://upmostly.com/tutorials/build-a-todo-app-in-react-using-hooks
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -30,9 +30,13 @@ function App() {
 
   function removeTodoAtIndex(i) {
     if (i === 0 && todos.length === 1) return;
-    setTodos(todos => todos.slice(0, i).concat(todos.slice(i +1, todos.length)));
+    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
     setTimeout(() => {
-      document.forms[0].elements[i-1].focus();
+      if (i === 0) {
+        document.forms[0].elements[i].focus()
+        } else {
+        document.forms[0].elements[i-1].focus();
+        }
     }, 0);
   }
   
